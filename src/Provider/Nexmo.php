a5c126b29390af14542487919ff89c082acfc9c9
Dmitry Patsura <talk@dmtry.me>
2015-10-31 20:43:25 +0300

    Start writting project

---
+++ src/Provider/Nexmo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\SMS\Provider;
+
+class Nexmo implements ProviderInterface
+{
+
+}
---
+++ src/Provider/ProviderInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\SMS\Provider;
+
+
+interface ProviderInterface
+{
+
+}
