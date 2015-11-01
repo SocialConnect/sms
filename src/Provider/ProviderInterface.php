d9473e8f5ec3bd72caaf34f403d45c10220ae07b
Dmitry Patsura <talk@dmtry.me>
2015-11-01 19:25:15 +0300

    ProviderInterface - added __construct method

--- src/Provider/ProviderInterface.php
+++ src/Provider/ProviderInterface.php
@@ -6,8 +6,16 @@
 namespace SocialConnect\SMS\Provider;
 
 
+use SocialConnect\Common\Http\Client\ClientInterface;
+
 interface ProviderInterface
 {
+    /**
+     * @param array $configuration Configuration for provider
+     * @param ClientInterface $httpClient Http client for requesting data
+     */
+    public function __construct(array $configuration, ClientInterface $httpClient);
+
     /**
      * @return float
      */
