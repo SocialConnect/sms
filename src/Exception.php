261594b5e846fea5313eec927183d8c4a88b5a94
Dmitry Patsura <talk@dmtry.me>
2015-11-06 20:43:08 +0300

    Added base Exception

---
+++ src/Exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\SMS;
+
+use Exception as BaseException;
+
+class Exception extends BaseException
+{
+
+}
